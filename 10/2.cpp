094b5fa8b7f05f5432ecde2270dc12c651205786
st142157 <yddats@mail.ru>
2025-10-18 22:50:16 +0300

    Add files via upload

---
+++ 10/2.cpp
@@ -0,0 +1,30 @@
+﻿#include<cstdio>
+
+int main(int argc, char** argv)
+{
+	double x = 0;
+	int y = 0;
+	int c = 1;
+	int z = 0;
+
+	scanf_s("%d", &z);
+	scanf_s("%d", &y);
+
+	x = z;
+
+	while ((y - x) > 0)
+	{
+		
+		x = x + x*15/100;
+		c = c + 1;
+
+		
+	
+	}
+
+
+	printf("%d", c);
+	
+
+	return 0;
+}
---
+++ 10/7.cpp
@@ -0,0 +1,32 @@
+﻿#include<cstdio>
+
+int main(int argc, char** argv)
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	int d = 0;
+	int x = 0;
+
+	scanf_s("%d", &a);
+	scanf_s("%d", &b);
+	scanf_s("%d", &c);
+	scanf_s("%d", &d);
+
+	
+
+	for (x = -100; x != 100; x++)
+	{
+		int p = (((x ^ 3) * a) + ((x ^ 2) * b) + (x * c) + d);
+
+		if ( p == 0)
+		{
+			printf("%d", x);
+		
+		}
+		
+	}
+	
+
+	return 0;
+}
