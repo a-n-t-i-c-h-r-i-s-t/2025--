b86c13e4032074ce9fc86af90262338c793c9953
st142157 <yddats@mail.ru>
2025-10-18 22:46:40 +0300

    Add files via upload

---
+++ 14/1.cpp
@@ -0,0 +1,44 @@
+﻿#include<cstdio>
+
+int main(int argc, char** argv)
+{
+	int a = 0;
+	int b = 1;
+	int v = 0;
+	int k = 1;
+	int z = 0;
+
+	scanf_s("%d", &a);
+
+	if ((a == 0) || (a == 1))
+	{
+		printf("%d", a);
+	}
+	else
+	{
+		while (b != a)
+		{
+			z = v + k;
+
+
+			if (v < k)
+			{
+				v = z;
+			}
+			else if (k < v)
+			{
+				k = z;
+			}
+			else if (k == v)
+			{
+				v = z;
+			}
+			
+			b = b + 1;
+		}
+
+		printf("%d", z);
+	}
+
+	return 0;
+}
