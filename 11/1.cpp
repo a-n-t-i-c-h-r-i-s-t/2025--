2b48a87ab0694772abcfed01d8c03a5233eb8bd8
st142157 <yddats@mail.ru>
2025-11-08 21:37:23 +0300

    Add files via upload

---
+++ 11/1.cpp
@@ -0,0 +1,36 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+    int n = 0;
+    scanf_s("%d", &n);
+
+    int* arr = (int*)malloc(n * sizeof(int));
+
+    int i = 0;
+
+    while (i != n)
+    {
+        scanf_s("%d", &arr[i]);
+
+        i++;
+    }
+    
+    i = 0;
+
+    while (i != n)
+    {
+
+        if (i % 2 == 0)
+        {
+            printf("%d", arr[i]);
+        }
+
+        i++;
+    }
+
+
+    free(arr);
+    return 0;
+}
---
+++ 11/7.cpp
@@ -0,0 +1,48 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+    int n = 0;
+    scanf_s("%d", &n);
+
+    int* arr = (int*)malloc(n * sizeof(int));
+
+    int i = 0;
+
+    while (i != n)
+    {
+        scanf_s("%d", &arr[i]);
+
+        i++;
+    }
+
+    i = 0;
+    int r = 0;
+    int z = 0;
+    int w = n;
+
+    while (i != w)
+    {
+        r = arr[i];
+        z = arr[w];
+        arr[i] = z;
+        arr[w] = r;
+
+        i++;
+        w--;
+
+    }
+
+    i = 0;
+    while (i != n)
+    {
+        printf("!d", arr[i]);
+        i++;
+    }
+
+    
+
+    free(arr);
+    return 0;
+}
