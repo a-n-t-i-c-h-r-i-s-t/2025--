05bb3ee813b7e7050f4ea21bd99034b2c0e75fef
st142157 <yddats@mail.ru>
2025-12-14 16:58:38 +0300

    Add files via upload

---
+++ 11.08-Homework6/4.cpp
@@ -0,0 +1,38 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv)
+{
+    int n = 0;
+    scanf_s("%d", &n);
+
+    int* arr = (int*)malloc(n * sizeof(int));
+
+    int i = 0;
+
+    while (i != n)
+    {
+        scanf_s("%d", &arr[i]);
+
+        i++;
+    }
+
+    i = 0;
+    int s = 0;
+
+    while (i != n)
+    {
+
+        if (arr[i] < arr[i+1])
+        {
+            s++;
+        }
+
+        i++;
+    }
+
+    printf("%d", s);
+
+    free(arr);
+    return 0;
+}
